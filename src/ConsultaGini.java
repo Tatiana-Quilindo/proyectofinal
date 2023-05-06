56c65afcaea3ede41b8e71587495b1ac5a6ec681
Tatiana-Quilindo <126364394+Tatiana-Quilindo@users.noreply.github.com>
2023-05-05 23:50:45 -0500

    Initial commit

---
+++ src/ConsultaGini.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class ConsultaGini {
+    private JButton button1;
+    private JPanel panel1;
+    private JTextField textField1;
+}
---
+++ src/consultaG.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class consultaG {
+    private JTextField paísTextField;
+    private JPanel LabelPais;
+    private JTextField segundoPaísTextField;
+    private JButton buscarButton;
+    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane2;
+    private JTextArea informaciónPaísTextArea;
+    private JTextArea informaciónPaisTextArea;
+    private JTextPane imagenBanderaTextPane;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JEditorPane imagenEscudoEditorPane;
+    private JTextArea interaccionConChatGPTTextArea;
+    private JTextPane imagenBanderaTextPane1;
+    private JTextArea imagenEscudoTextArea;
+    private JTextPane interacciónChatGPTTextPane;
+}
