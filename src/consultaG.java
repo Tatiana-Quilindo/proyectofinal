881a8dbc587e036cb8190d817d5eb7321563db87
Tatiana-Quilindo <126364394+Tatiana-Quilindo@users.noreply.github.com>
2023-05-06 13:56:37 -0500

    Prueba repo

--- src/consultaG.java
+++ src/consultaG.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class consultaG {
     private JTextField paísTextField;
-    private JPanel LabelPais;
+    public JPanel panel1;
     private JTextField segundoPaísTextField;
     private JButton buscarButton;
     private JTabbedPane tabbedPane1;
@@ -17,4 +19,13 @@ public class consultaG {
     private JTextPane imagenBanderaTextPane1;
     private JTextArea imagenEscudoTextArea;
     private JTextPane interacciónChatGPTTextPane;
+public consultaG() {
+    buscarButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("ahora siiiiiiiiiiiii!");
+
+        }
+    });
+}
 }
